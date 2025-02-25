ed3af2187ac223974b469e85e2d17217d22d100d
renatocamp <renatocontato@hotmail.com>
2025-02-24 21:56:14 -0300

    Validação no FuncionarioDTO

--- src/main/java/br/com/orion/registroponto/dtos/FuncionarioDTO.java
+++ src/main/java/br/com/orion/registroponto/dtos/FuncionarioDTO.java
@@ -1,9 +1,18 @@
 package br.com.orion.registroponto.dtos;
 
+import org.hibernate.validator.constraints.Length;
+
 import br.com.orion.registroponto.enums.StatusFuncionario;
 import br.com.orion.registroponto.models.Funcionario;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
-public record FuncionarioDTO(String nome, String senha, String endereco, String email, String telefone, StatusFuncionario status) {
+public record FuncionarioDTO(@NotNull String nome, 
+							 @NotNull @Length(min = 8) String senha, 
+							 @NotNull String endereco, 
+							 @Email @NotNull String email, 
+							 @NotNull String telefone, 
+							 @NotNull StatusFuncionario status) {
 	
 	public Funcionario toFuncionario() {
 		

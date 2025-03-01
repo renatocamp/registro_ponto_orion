fe80abe107a4371b8ebb66e3bd2c934d465c0f4f
renatocamp <renatocontato@hotmail.com>
2025-03-01 14:44:45 -0300

    Teste de integração para listar e atualizar status para Inativo

--- src/test/java/br/com/orion/registroponto/api_integracao/FuncionarioTesteAPI.java
+++ src/test/java/br/com/orion/registroponto/api_integracao/FuncionarioTesteAPI.java
@@ -1,9 +1,13 @@
 package br.com.orion.registroponto.api_integracao;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.orion.registroponto.dtos.FuncionarioDTO;
 import br.com.orion.registroponto.enums.StatusFuncionario;
+import br.com.orion.registroponto.models.Funcionario;
 import br.com.orion.registroponto.services.impl.FuncionarioServiceImpl;
 
 @AutoConfigureMockMvc
@@ -32,6 +37,24 @@ public class FuncionarioTesteAPI {
     
     @Autowired
     private ObjectMapper objectMapper;
+    
+    private List<Funcionario> lista;
+    
+    @BeforeEach
+    public void setup() {
+    	
+    	 lista = new ArrayList<>();
+    	
+    	Funcionario f1 = new Funcionario();
+    	f1.setNome("Renato");
+    	
+    	Funcionario f2 = new Funcionario();
+    	f1.setNome("Pedro");
+    	
+    	lista.add(f1);
+    	lista.add(f2);
+    	
+    }
     
     @Test
     public void deveCadastrarFuncionario() throws Exception {
@@ -77,6 +100,43 @@ public class FuncionarioTesteAPI {
     			.content(objectMapper.writeValueAsString(funcionarioDuplicado)))
     	.andExpect(status().isConflict());
     	
+    }
+    
+    @Test
+    public void deveDeixarFuncionarioInativo() throws  Exception {
+    	
+    	FuncionarioDTO funcionarioInativo = new FuncionarioDTO("Renato Campos",
+                "senha123",
+                "Rua Maranhão, 123",
+                "renato@email.com",
+                "11999999999",
+                StatusFuncionario.ATIVO
+                );
+    	
+    	Mockito.when(funcionarioService.findByEmail(funcionarioInativo.email()))
+    	.thenReturn(Optional.of(funcionarioInativo.toFuncionario()));
+    	
+    	mvc.perform(MockMvcRequestBuilders.put("/desativar")
+    			.contentType(MediaType.APPLICATION_JSON)
+    			.content(objectMapper.writeValueAsString(funcionarioInativo)))
+    	.andExpect(status().isOk());
+    }
+    
+    @Test
+    public void deveListarTodosFuncionarios() throws Exception {
+    	
+    	Mockito.when(funcionarioService.listarTodosFuncionarios()).thenReturn(lista);
+    	
+    	mvc.perform(MockMvcRequestBuilders.get("/listar")
+    			.contentType(MediaType.APPLICATION_JSON)
+    			.content(objectMapper.writeValueAsString(lista)))
+    	.andExpect(status().isOk())
+    	.andExpect(resultado -> {
+    		
+    		String resposta = resultado.getResponse().getContentAsString();
+    		System.out.println(resposta);
+    	})
+    	.andExpect(jsonPath("$.length()").value(2));
     }
 
 }
